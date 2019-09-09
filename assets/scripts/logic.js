bea9047ba846e0f31438a72cf66383c63f4992e0
Michael Stevens <okyfunoky@hotmail.com>
2019-09-09 08:15:12 -0700

    Basic function structure

---
+++ assets/scripts/logic.js
@@ -0,0 +1,25 @@
+var initialAnimalArray = ["dog","cat","pengiun","macaque"];
+
+function createButtons(){
+    initialAnimalArray.forEach(animal =>{
+        createButton(animal);
+    });
+}
+
+function createButton(animal){
+    //create button
+    //add animal name as value
+    //add animalbutton class
+    //attach button
+}
+
+function populateGifs(animal){
+    //hit api
+    //parse data
+    //update html
+}
+
+function clearGifs(){
+
+}
+
